cbb1526b5e38bcad3d866eaf4919113d14cbe82c
Steven Heidel <steven@livingskyweb.ca>
2013-04-08 16:38:51 -0600

    Drop in Rails and Bootstrap

---
+++ backend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EncoreMvp::Application.config.secret_token = '8d0c2ad37742388c8935fd2e3b55f9e262bf4a011122db4b2fe91986a7ed0255d9ffe81b994563a75ede4be8d19e0391455031c4e3588a0126a8b2e378f46543'
