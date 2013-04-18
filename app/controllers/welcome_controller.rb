b63d8e8d08273db0f65fcfcbd29afb67121327fd
Steven Heidel <steven@livingskyweb.ca>
2013-04-18 15:31:39 -0600

    Try adding in the menu

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,11 @@
 class WelcomeController < ApplicationController
-  before_filter :authenticate_user!, :except => [:index]
+  before_filter :authenticate_user!, :except => :index
 
   def index
     redirect_to :action => :profile if user_signed_in?
   end
 
   def profile
-    redirect_to :action => :profile unless user_signed_in?
-
     @concerts = Concert.all
   end
 end
