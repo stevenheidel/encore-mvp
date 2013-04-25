88f873c997eff5e24bf0fe839149ff81db632c34
Steven Heidel <steven@livingskyweb.ca>
2013-04-25 11:57:29 -0600

    Add updated Gemfile.lock to git

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -7,8 +7,8 @@ AssetSync.configure do |config|
   # Increase upload performance by configuring your region
   # config.fog_region = 'eu-west-1'
   #
-  # Don't delete files from the store
-  # config.existing_remote_files = "keep"
+  # Delete files from the store
+  config.existing_remote_files = "delete"
   #
   # Automatically replace files with their equivalent gzip compressed version
   config.gzip_compression = true
