fe63c8a02627a32dc7457bbf3f3cf077edb44fbd
Steven Heidel <steven@livingskyweb.ca>
2013-04-29 17:14:07 -0600

    Bug fixes surrounding default profile.

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,10 +1,10 @@
 class ConcertsController < InheritedResources::Base
-  before_filter :authenticate_user!
-
   def show
-    # Add concert to current user
-    concert = Concert.find_by_id(params[:id])
-    current_user.add_concert(concert)
+    if user_signed_in?
+      # Add concert to current user
+      concert = Concert.find_by_id(params[:id])
+      current_user.add_concert(concert)
+    end
 
     show!
   end
