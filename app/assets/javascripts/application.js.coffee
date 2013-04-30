e41ea2c92152741d5c8e6c92da05dd4b22116414
Steven Heidel <steven@livingskyweb.ca>
2013-04-30 01:43:27 -0600

    Remove old javascripts so it works on Heroku

--- app/assets/javascripts/application.js.coffee
+++ app/assets/javascripts/application.js.coffee
@@ -1,7 +1,7 @@
 #= require jquery
 #= require jquery_ujs
-#= require jquery.mobile
 
+#= require jquery.mobile
 #= require jquery.jpanelmenu
 
 #= require encore
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ EncoreMvp::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true # change this back
+  config.assets.compile = true # TODO: change this back
 
   # Generate digests for assets URLs
   config.assets.digest = true
