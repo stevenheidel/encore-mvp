20200b7d93d092891a90be3b6d7ec8ec5055f12d
Steven Heidel <steven@livingskyweb.ca>
2013-04-30 18:32:30 -0600

    turn off new relic for activeadmin and fix having no touch icons yet

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -123,7 +123,9 @@ ActiveAdmin.setup do |config|
   # You can add before, after and around filters to all of your
   # Active Admin resources and pages from here.
   #
-  # config.before_filter :do_something_awesome
+  config.before_filter do
+    newrelic_ignore if Rails.env.production?
+  end
 
 
   # == Register Stylesheets & Javascripts
