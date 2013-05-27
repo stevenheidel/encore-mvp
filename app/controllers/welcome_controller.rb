d20a56a6325b91b8f46f50b4f01b6b40a1e82ace
Steven Heidel <steven@livingskyweb.ca>
2013-05-26 22:51:21 -0400

    Always add the current concert to the profile

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,12 @@ class WelcomeController < ApplicationController
   skip_before_filter :authenticate_user!, :only => :index
 
   def index
-    redirect_to :action => :profile if user_signed_in?
+    if user_signed_in?
+      concert = Concert.find_by_id(ENV['CURRENT_CONCERT_ID'].to_i)
+      current_user.add_concert(concert)
+
+      redirect_to :action => :profile
+    end
   end
 
   def profile
